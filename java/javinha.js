1ff6020c46cecdac9da0246449f958920abc6407
Kevin Romeiro <67075301+Kevin-028@users.noreply.github.com>
2021-05-19 11:31:54 -0300

    Versão final

--- java/javinha.js
+++ java/javinha.js
@@ -1,12 +1,16 @@
 
 $(document).ready(function () {
-    var altura_viewport = $(window).height();
-    var altura_Elemento = $(document).height();
-    var distancia_topo  = $("#main").offset().top;
-    var diferenca       = altura_Elemento - altura_viewport;
+    
     
     $(window).scroll(function () { 
-        var posicao_scroll = $(window).scrollTop();
+
+        var altura_viewport = $(window).height(); // Tamanho da tela
+        var altura_Elemento = $(document).height(); // Altura do meu documento
+        var distancia_topo  = $("#main").offset().top; // Altura da minha teg mãe
+        var diferenca       = altura_Elemento - altura_viewport; // diferença da altura da tela para o documento
+
+        
+        var posicao_scroll = $(window).scrollTop();  // em queponto do documento o campo visivel esta
         /* Verificando se eles estão dentro do seu campo de aparição */ 
         var rodape_on1;
         var rodape_on2;
@@ -15,16 +19,15 @@ $(document).ready(function () {
         }else{
             rodape_on1 = false;
         }
-        if (posicao_scroll >= (50-distancia_topo + diferenca)){
+        if (posicao_scroll >= (-400-distancia_topo + diferenca)){
             rodape_on2 = true;
+
         }else{
             rodape_on2 = false;
         }
         /* fim da verificação */
 
-        var tipo = $("#RodapeFixo").css("display");
-
-
+        var tipo = $("#RodapeFixo").css("display"); //estou verificando oque esta sendo atribuido a ID referente a display
         if ((rodape_on1 || rodape_on2) == true){
             if ( tipo != "flex"){
                 $("#RodapeFixo").slideDown();
@@ -64,3 +67,17 @@ $("#ancoraplanos ").click(function() {
     $(window).scrollTop(posicao);
 })
 
+$("#ancorareduza").click(function(){
+    let posicao = $(".ReduzaCustos").offset().top - $("nav").height() -15;
+    $(window).scrollTop(posicao);
+});
+
+$("#ancoraespaco").click(function(){
+    let posicao = $(".ReduzaCustos").offset().top - $("nav").height() -15;
+    $(window).scrollTop(posicao);
+});
+
+$("#ancoramissao").click(function(){
+    let posicao = $(".Missao").offset().top - $("nav").height() -0;
+    $(window).scrollTop(posicao);
+});
